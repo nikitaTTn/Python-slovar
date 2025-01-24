28a5322f372ad6e38ec7c92e566714a69ee58478
nikitaTTn <kote72642@gmail.com>
2025-01-24 15:14:00 +0500

    333

--- final_win.py
+++ final_win.py
@@ -1,23 +1,22 @@
 from PyQt5.QtWidgets import *
-from inst import *
 
 class FinalWin(QWidget):
     def __init__(self, score):
         super().__init__()
         self.score = score
         self.setGeometry(100, 100, 400, 200)
-        self.setWindowTitle(txt_finalwin)
+        self.setWindowTitle("Результаты теста по словарям")
         self.initUI()
         self.show()
 
     def initUI(self):
         layout = QVBoxLayout()
 
+        dict_knowledge_text = "Отлично! Вы хорошо знаете словари в Python." if self.score["dict_knowledge"] > 3 else "Вам нужно подтянуть знания по словарям."
 
-        extroversion_text = txt_extroversion_high if self.score["extroversion"] > 2 else txt_extroversion_low
-        neuroticism_text = txt_neuroticism_high if self.score["neuroticism"] > 1 else txt_neuroticism_low
+        dict_practice_text = "Вы отлично справляетесь с практическими задачами по словарям!" if self.score["dict_practice"] > 2 else "Практические навыки работы со словарями требуют улучшения."
 
-        layout.addWidget(QLabel(extroversion_text))
-        layout.addWidget(QLabel(neuroticism_text))
+        layout.addWidget(QLabel(dict_knowledge_text))
+        layout.addWidget(QLabel(dict_practice_text))
 
         self.setLayout(layout)
--- my_app.py
+++ my_app.py
@@ -1,29 +1,29 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
-from second_win import TestWin
+from theory_win import TheoryWin
 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 400, 200)
-        self.setWindowTitle("Личностный тест Айзенка")
+        self.setGeometry(100, 100, 600, 400)
+        self.setWindowTitle("Тест по словарям в Python")
         self.initUI()
         self.connects()
         self.show()
 
     def initUI(self):
-        self.btn_next = QPushButton("Начать тест", self)
-        self.hello_text = QLabel("Добро пожаловать в личностный тест Айзенка!")
-        self.instruction = QLabel("Этот тест поможет определить ваш уровень экстраверсии и нейротизма.")
+        self.btn_next = QPushButton("Начать обучение", self)
+        self.hello_text = QLabel("Добро пожаловать на тест по словарям в Python!")
+        self.instruction = QLabel("Этот тест поможет вам проверить свои знания по работе со словарями в Python.")
 
         self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.instruction, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
         self.setLayout(self.layout_line)
 
     def next_click(self):
-        self.tw = TestWin()
+        self.tw = TheoryWin()
         self.hide()
 
     def connects(self):
